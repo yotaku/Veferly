28b2f537e5fbb100bfa92c0ee5581b3158fda87f
yotaku <kaidesuyo8864@gmail.com>
2025-06-09 19:48:00 +0900

    Create encrypt.js

---
+++ encrypt.js
@@ -0,0 +1,24 @@
+// encrypt.js
+const crypto = require('crypto');
+
+const ALGORITHM = 'aes-256-cbc';
+const KEY = crypto.createHash('sha256').update(process.env.ENCRYPTION_KEY).digest();
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv(ALGORITHM, KEY, iv);
+  const encrypted = Buffer.concat([cipher.update(text, 'utf8'), cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+function decrypt(data) {
+  const [ivHex, encryptedHex] = data.split(':');
+  const iv = Buffer.from(ivHex, 'hex');
+  const encrypted = Buffer.from(encryptedHex, 'hex');
+  const decipher = crypto.createDecipheriv(ALGORITHM, KEY, iv);
+  const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+  return decrypted.toString('utf8');
+}
+
+module.exports = { encrypt, decrypt };
